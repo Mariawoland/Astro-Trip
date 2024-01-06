6e41cc1cf811062b71df63c4670e83642fa23ede
Mariam Ekizashvili <122484329+Mariawoland@users.noreply.github.com>
2024-01-07 02:25:02 +0400

    Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -23,19 +23,19 @@ const Hero = () => {
                 className="mySwiper w-full h-full"
             >
                 <SwiperSlide>
-                    <img src='hero.JPG' className='h-full w-full absolute inset-0 object-cover md:brightness-[0.35] brightness-[0.2] -z-10 mizumeba' />
+                    <img src='hero.jpg' className='h-full w-full absolute inset-0 object-cover md:brightness-[0.35] brightness-[0.2] -z-10 mizumeba' />
 
                     <h1 className='text-white-1 font-black xl:text-[4.5rem] lg:text-[3.75rem] md:text-[2.8rem] text-[3.75rem] z-[20] pt-[35vh] md:ps-[8%] ps-[10%] leading-[160%]'>Discover <br className='md:hidden flex' /> New <br /> Horizons</h1>
                 </SwiperSlide>
 
                 <SwiperSlide>
-                    <img src='services1.JPG' className='h-full w-full absolute inset-0 object-cover md:brightness-[0.35] brightness-[0.2] -z-10 mizumeba' />
+                    <img src='services1.jpg' className='h-full w-full absolute inset-0 object-cover md:brightness-[0.35] brightness-[0.2] -z-10 mizumeba' />
 
                     <h1 className='text-white-1 font-black xl:text-[4.5rem] lg:text-[3.75rem] md:text-[2.8rem] text-[3.75rem] z-[20] md:pt-[45vh] pt-[35vh] md:ps-[8%] px-[10%] leading-[160%]'>Explore <br className='sm:hidden flex' /> The <br className='sm:hidden flex' /> Galaxy</h1>
                 </SwiperSlide>
 
                 <SwiperSlide>
-                    <img src='services2.JPG' className='h-full w-full absolute inset-0 object-cover md:brightness-[0.35] brightness-[0.2] -z-10 mizumeba' />
+                    <img src='services2.jpg' className='h-full w-full absolute inset-0 object-cover md:brightness-[0.35] brightness-[0.2] -z-10 mizumeba' />
 
                     <h1 className='text-white-1 font-black xl:text-[4.5rem] lg:text-[3.75rem] md:text-[2.8rem] text-[3.75rem] z-[20] pt-[35vh] md:ps-[8%] ps-[10%] leading-[160%]'>Break <br className='md:hidden flex' /> Throgh <br /> Unknown</h1>
                 </SwiperSlide>
