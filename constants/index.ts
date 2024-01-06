eb0ea110348a60280669a11721721475fbd83c04
Mariam Ekizashvili <122484329+Mariawoland@users.noreply.github.com>
2024-01-07 02:28:43 +0400

    index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -6,8 +6,8 @@ export const SERVICES = [
         date: 'Every Friday and Saturday evening',
         shortDescription: "Take a guided hike along our Sunset Trail, offering breathtaking views as the sun dips below the horizon.",
         longDescription: "As the sun sets, embark on a celestial adventure under the expert guidance of our astronomy team. Our stargazing night tour offers an intimate experience, limited to a small group, ensuring personalized attention. Following the tour, cozy up with blankets provided as you enjoy hot cocoa and engage in celestial conversations around a warming bonfire. Contact for booking.",
-        photo1: "/services/1-1.JPG",
-        photo2: "/services/1-2.WEBP"
+        photo1: "/services/1-1.jpg",
+        photo2: "/services/1-2.webp"
     },
     {
         title: 'Solar System Exploration',
@@ -15,8 +15,8 @@ export const SERVICES = [
         date: 'Monthly, check our schedule',
         shortDescription: "Take a guided hike along our Sunset Trail, offering breathtaking views as the sun dips below the horizon.",
         longDescription: "Perfect for aspiring astronomers of all ages, our Solar System Exploration Workshop combines interactive learning with captivating discussions. Participants will have the chance to create their own scale models of planets, explore the wonders of our neighboring celestial bodies, and gain a deeper understanding of our place in the vast universe. Light refreshments will be provided to fuel the quest for knowledge.",
-        photo1: "/services/2-1.WEBP",
-        photo2: "/services/2-2.JPG"
+        photo1: "/services/2-1.webp",
+        photo2: "/services/2-2.jpg"
     },
     {
         title: 'Sunrise and Meditation',
@@ -24,8 +24,8 @@ export const SERVICES = [
         date: 'Weekly on Sundays',
         shortDescription: "Take a guided hike along our Sunset Trail, offering breathtaking views as the sun dips below the horizon.",
         longDescription: "Rise with the sun and invigorate your body and mind with our Sunrise Yoga and Meditation experience. Led by a certified instructor, the session takes place in a serene outdoor location with panoramic views. Participants will be provided with yoga mats and blankets, ensuring a comfortable and tranquil start to the day.",
-        photo1: "/services/3-1.JPG",
-        photo2: "/services/3-2.WEBP"
+        photo1: "/services/3-1.jpg",
+        photo2: "/services/3-2.webp"
     },
     {
         title: 'Astrophotography Workshop',
@@ -33,8 +33,8 @@ export const SERVICES = [
         date: 'Bi-monthly, check our schedule',
         shortDescription: "Take a guided hike along our Sunset Trail, offering breathtaking views as the sun dips below the horizon.",
         longDescription: "Unleash your inner photographer and learn the art of capturing stunning celestial images. Our Astrophotography Workshop covers camera settings, composition techniques, and post-processing tips. Participants are encouraged to bring their cameras and tripods, and our experienced instructors will guide them through the process. Light snacks and refreshments will be provided.",
-        photo1: "/services/4-1.WEBP",
-        photo2: "/services/4-2.JPG"
+        photo1: "/services/4-1.webp",
+        photo2: "/services/4-2.jpg"
     },
     {
         title: 'Romantic Celestial Dinner',
@@ -42,8 +42,8 @@ export const SERVICES = [
         date: 'By reservation',
         shortDescription: "Take a guided hike along our Sunset Trail, offering breathtaking views as the sun dips below the horizon.",
         longDescription: "Elevate your dining experience with our Romantic Celestial Dinner package. Begin the evening with a guided stargazing session, followed by an exquisite three-course meal served at an intimate outdoor setting. Savor each bite under the gentle glow of fairy lights, and let the celestial wonders above set the mood for an unforgettable evening.",
-        photo1: "/services/5-1.JPG",
-        photo2: "/services/5-2.JPEG"
+        photo1: "/services/5-1.jpg",
+        photo2: "/services/5-2.jpeg"
     },
     {
         title: 'Guided Hiking Adventure ',
@@ -51,8 +51,8 @@ export const SERVICES = [
         date: 'Every Wednesday evening',
         shortDescription: "Take a guided hike along our Sunset Trail, offering breathtaking views as the sun dips below the horizon.",
         longDescription: "Lace up your hiking boots and embark on a journey through nature's canvas. Our Sunset Trail hike is led by knowledgeable guides who share insights into the flora and fauna of the area. As you ascend, witness the changing hues of the sky, creating a mesmerizing backdrop for an evening of exploration. Snacks and water bottles are provided for your comfort during the hike.",
-        photo1: "/services/6-1.JPG",
-        photo2: "/services/6-1.WEBP"
+        photo1: "/services/6-1.jpg",
+        photo2: "/services/6-1.webp"
     },
     {
         title: 'Guided Hiking Adventure',
@@ -60,8 +60,8 @@ export const SERVICES = [
         date: 'Every Wednesday evening',
         shortDescription: "Take a guided hike along our Sunset Trail, offering breathtaking views as the sun dips below the horizon.",
         longDescription: "Lace up your hiking boots and embark on a journey through nature's canvas. Our Sunset Trail hike is led by knowledgeable guides who share insights into the flora and fauna of the area. As you ascend, witness the changing hues of the sky, creating a mesmerizing backdrop for an evening of exploration. Snacks and water bottles are provided for your comfort during the hike.",
-        photo1: "/services/7-1.JPG",
-        photo2: "/services/7-2.JPG"
+        photo1: "/services/7-1.jpg",
+        photo2: "/services/7-2.jpg"
     },
     {
         title: 'Stargazer`s Picnic',
@@ -69,8 +69,8 @@ export const SERVICES = [
         date: 'Monthly, check our schedule',
         shortDescription: "Take a guided hike along our Sunset Trail, offering breathtaking views as the sun dips below the horizon.",
         longDescription: "Immerse yourself in a celestial dining experience with our Stargazers' Picnic. Lay out your blanket under the vast night sky and indulge in a curated selection of gourmet delights. From savory bites to sweet treats, each item is carefully chosen to complement the celestial ambiance. Telescopes will be available for post-picnic stargazing, creating a perfect blend of culinary and cosmic delights.",
-        photo1: "/services/8-1.JPG",
-        photo2: "/services/8-2.WEBP"
+        photo1: "/services/8-1.jpg",
+        photo2: "/services/8-2.webp"
     },
     {
         title: 'Full Moon Hike',
@@ -78,8 +78,8 @@ export const SERVICES = [
         date: 'Night of the full moon',
         shortDescription: "Take a guided hike along our Sunset Trail, offering breathtaking views as the sun dips below the horizon.",
         longDescription: "As the full moon illuminates the landscape, embark on a guided hike through moonlit trails. Upon returning, gather around a crackling campfire for stories and camaraderie. Marshmallow roasting, hot chocolate, and the soothing sounds of nature complete this enchanting experience. This event is perfect for nature lovers seeking a tranquil evening under the lunar glow.",
-        photo1: "/services/9-1.JPG",
-        photo2: "/services/9-1.JPEG"
+        photo1: "/services/9-1.jpg",
+        photo2: "/services/9-1.jpeg"
     }
 ];
 
@@ -88,13 +88,13 @@ export const CABINS = [
         title: 'Stellar Retreat Cabin',
         price: '$150 per night',
         description: 'The Stellar Retreat Cabin is haven nestled in heart of nature, offering an immersive celestial experience.',
-        photo: "acc1.JPG"
+        photo: "acc1.jpg"
     },
     {
         title: 'Luna Luxe Skyview Lodge',
         price: '$250 per night',
         description: 'This premium accommodation features a luxurious bedroom, deck equipped with a telescope, and a modern living space.',
-        photo: "acc2.JPG"
+        photo: "acc2.jpg"
     }
 ]
 
